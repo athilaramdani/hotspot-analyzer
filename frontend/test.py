cdd9a7b85a3207f5b7a84bb45378d8fe0432d729
Athila Ramdani Saputra <68465902+athilaramdani@users.noreply.github.com>
2025-06-23 20:36:16 +0700

    inisialsiasia

--- frontend/test.py
+++ frontend/test.py
@@ -1,1 +1,20 @@
-print("this is model")
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget
+import sys
+
+def show_gui():
+    app = QApplication(sys.argv)
+
+    window = QWidget()
+    window.setWindowTitle("Tes GUI Pertama")
+    window.setGeometry(100, 100, 300, 150)
+
+    label = QLabel("Halo dari PyQt!", window)
+    button = QPushButton("Klik Aku", window)
+
+    layout = QVBoxLayout()
+    layout.addWidget(label)
+    layout.addWidget(button)
+    window.setLayout(layout)
+
+    window.show()
+    sys.exit(app.exec_())
